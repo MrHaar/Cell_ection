764bb729a40d55f7ce643389daf3b23972b4cd42
MrHaar <47451387+MrHaar@users.noreply.github.com>
2019-04-25 15:16:01 +0200

    Add files via upload

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -1,7 +1,12 @@
 @section('content')
 <div class="content-wrapper">
 	<div class="container">
-		<h2>Coming soon...</h2>
+		<h2>All the data we use on this site is available here</h2>
+
+		<a href="http://cellection.univ-lyon1.fr/downloadable/cline.rar" download>
+		  <button class="btn" style="width:100%"><i class="fa fa-download"></i> Download</button>
+		</a>
+
 	</div>
 </div>
 @stop
